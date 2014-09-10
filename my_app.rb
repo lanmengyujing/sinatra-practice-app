9642afb1b0dad04156d9675ec9beee46f0122710
Liu Jing <liujing92684@gmail.com>
2014-09-10 23:24:49 +1000

    enable user register and login

--- my_app.rb
+++ my_app.rb
@@ -5,23 +5,95 @@ class MyApp < Sinatra::Base
   set :sessions, true
   set :foo, 'bar'
   set :static, true
-  set :public_dir, File.dirname(__FILE__) + '/public/views'
 
-  get '/show-task' do
-    #'Hello world!'
+  set :username, ''
+  set :token, 'maketh1$longandh@rdtoremember'
+  set :password, ''
+
+  helpers do
+    def admin?
+      request.cookies[settings.username] == settings.token
+    end
+
+    def signed_up?
+      !settings.username.nil?
+    end
+
+    def protected!
+      halt [401, 'Not Authorized'] unless admin?
+    end
+  end
+
+  get '/' do
     slim :index
   end
 
-  get '/:task' do
-    @task = params[:task].split('-').join(' ').capitalize
-    slim :task
+  get '/admin' do
+    slim :admin
   end
 
-  post '/show-task' do
-    @task =  params[:task]
-    slim :task
+  post '/login' do
+    if params['username']==settings.username&&params['password']==settings.password
+      response.set_cookie(settings.username, settings.token)
+      redirect '/private'
+    else
+      "Username or Password incorrect"
+    end
   end
 
+  get '/logout' do
+    response.set_cookie(settings.username, false)
+    redirect '/'
+  end
+
+  get '/private' do
+    protected!
+    'For Your Eyes Only!'
+  end
+
+  get '/sign' do
+    slim :sign
+  end
+
+  post '/sign' do
+    settings.username = params['username']
+    settings.password = params['password']
+    redirect '/'
+  end
+
+#register do
+#  def auth (type)
+#    condition do
+#      redirect "/login" unless send("is_#{type}?")
+#    end
+#  end
+#end
+#
+#helpers do
+#  def is_user?
+#    @user != nil
+#  end
+#end
+#
+#before do
+#  @user = User.get(session[:user_id])
+#end
+#
+#get "/" do
+#  "Hello, anonymous."
+#end
+#
+#get "/protected", :auth => :user do
+#  "Hello, #{@user.name}."
+#end
+#
+#post "/login" do
+#  session[:user_id] = User.authenticate(params).id
+#end
+#
+#get "/logout" do
+#  session[:user_id] = nil
+#end
 
 end
 
